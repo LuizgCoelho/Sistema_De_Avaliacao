9ff7f6e981902d183b96c292ce4bc3c62ab53b7a
Luiz Guilherme Ferreira Coelho <luizg.coelho1802@gmail.com>
2026-09-07 12:53:30 -0300

    feature: 8 ao 11 Etapa 7 main.c

--- main.c
+++ main.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "afinidade.h"
+
 int main()
 {
-    Pessoas pessoa;
+    Sistema sistema;
+    sistema.pessoas = NULL;
+    sistema.qtdCadastrados = 0;
+    
     int opcao;
 
-    do{
-       printf(" ========================================\n");
-         printf("SISTEMA DE AFINIDADES\n");
+    do {
+        printf("\n========================================\n");
+        printf("SISTEMA DE AFINIDADES\n");
         printf("========================================\n");
         printf("1 - Cadastrar pessoas\n");
         printf("2 - Exibir pessoas e preferencias\n");
@@ -19,31 +23,54 @@ int main()
         printf("7 - Analisar preferencias de duas pessoas\n");
         printf("0 - Encerrar\n");
         printf("Opcao:\n ");
-        scanf("%d",&opcao);
+        scanf("%d", &opcao);
 
-        switch(opcao){
-            case 1:CadastraPessoas(&pessoa);
+        switch(opcao) {
+            case 1:
+                CadastraPessoas(&sistema);
                 break;
-            case 2:ExibePessoas(&pessoa);
+            case 2:
+                ExibePessoas(&sistema);
                 break;
-            case 3:BuscaPessoas(pessoa.nome, pessoa.qtdCadastrados);
+            case 3: {
+                int indice = BuscaPessoas(sistema.pessoas, sistema.qtdCadastrados);
+                if (indice != -1) {
+                    printf("\n=================================================================\n");
+                    printf("%-15s%-6s%-6s%-6s%-6s%-6s%-6s\n", "NOME", "MUS", "CIN", "JOG", "ESP", "LEI", "PROG");
+                    printf("=================================================================\n");
+                    
+                    printf("%-15s", sistema.pessoas[indice].nome);
+                    for(int j = 0; j < QTD_PREF; j++) {
+                        printf("%-6.1f", sistema.pessoas[indice].notas[j]);
+                    }
+                    printf("\n=================================================================\n\n");
+                }
                 break;
-            case 4:ComparaPessoas(&pessoa, pessoa.qtdCadastrados);
+            }
+            case 4:
+                ComparaPessoas(&sistema);
                 break;
-            case 5:ComparaAfinidade(&pessoa, pessoa.qtdCadastrados);
+            case 5:
+                ComparaAfinidade(&sistema);
                 break;
-            case 6:ExibeRanking(&pessoa,pessoa.qtdCadastrados);
+            case 6:
+                ExibeRanking(&sistema);
                 break;
-
-            case 7:printf("\n");
+            case 7:
+                AnalisaPreferencia(&sistema);
                 break;
-            case 0:printf("\n");
-
+            case 0:
+                printf("\nEncerrando o sistema...\n");
                 break;
-            default:printf("essa opcao nao existe");
+            default:
+                printf("Essa opcao nao existe.\n");
         }
 
-    }while(opcao!=0);
+    } while(opcao != 0);
+
+    if (sistema.pessoas != NULL) {
+        free(sistema.pessoas);
+    }
 
     return 0;
 }
